e4a90b2d58eabd827327b28b474c7f8f20d85e3f
Thay483 <112909056+Thay483@users.noreply.github.com>
2022-12-05 20:39:57 -0300

    Arrumando o erro da view create de matérias

--- resources/views/materias/create.blade.php
+++ resources/views/materias/create.blade.php
@@ -2,87 +2,86 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<div class="row">
+    <div class="container">
+    <div class="row">
 
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h6>Adicionar Novo Curso</h6>
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h6>Adicionar Novo Curso</h6>
+            </div>
         </div>
-    </div>
-</div> -->
+    </div> -->
 
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-<form class="card-body" action="{{ route('materias.store') }}" method="POST">
-    @csrf
+    <form class="card-body" action="{{ route('materias.store') }}" method="POST">
+        @csrf
 
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Nome do Curso:</strong>
-                <input type="text" name="nome" class="form-control" placeholder="Nome do Curso">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nome do Curso:</strong>
+                    <input type="text" name="nome" class="form-control" placeholder="Nome do Curso">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Descrição Mínima do Curso:</strong>
+                    <input type="text" name="desc_minima" class="form-control" placeholder="Descrição Mínima do Curso">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Limite Mínimo de Alunos:</strong>
+                    <input type="text" name="lim_min" class="form-control" placeholder="Limite Mínimo de Alunos">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Limite Máximo de Alunos:</strong>
+                    <input type="text" name="lim_max" class="form-control" placeholder="Limite Máximo de Alunos">
+                </div>
+            </div>
+            <!-- <div class="col-xs-12 col-sm-12 col-md-12"> -->
+                <!-- <div class="form-group">
+                    <strong>ID do Aluno:</strong>
+                    <input type="text" name="id_aluno" class="form-control" placeholder="ID do Aluno">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>ID do Professor:</strong>
+                    <input type="text" name="professores_id" class="form-control" placeholder="ID do Professor">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nota do Aluno:</strong>
+                    <input type="text" name="notas_nota" class="form-control" placeholder="Nota do Aluno">
+                </div>
+            </div> -->
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Descrição Completa:</strong>
+                    <textarea class="form-control" name="desc_completa" placeholder="Descrição Completa"></textarea>
+                </div>
+            </div><br><br>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Descrição Mínima do Curso:</strong>
-                <input type="text" name="desc_minima" class="form-control" placeholder="Descrição Mínima do Curso">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Limite Mínimo de Alunos:</strong>
-                <input type="text" name="lim_min" class="form-control" placeholder="Limite Mínimo de Alunos">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Limite Máximo de Alunos:</strong>
-                <input type="text" name="lim_max" class="form-control" placeholder="Limite Máximo de Alunos">
-            </div>
-        </div>
-        <!-- <div class="col-xs-12 col-sm-12 col-md-12"> -->
-            <!-- <div class="form-group">
-                <strong>ID do Aluno:</strong>
-                <input type="text" name="id_aluno" class="form-control" placeholder="ID do Aluno">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>ID do Professor:</strong>
-                <input type="text" name="professores_id" class="form-control" placeholder="ID do Professor">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Nota do Aluno:</strong>
-                <input type="text" name="notas_nota" class="form-control" placeholder="Nota do Aluno">
-            </div>
-        </div> -->
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Descrição Completa:</strong>
-                <textarea class="form-control" name="desc_completa" placeholder="Descrição Completa"></textarea>
-            </div>
-        </div><br><br>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </div>
 
-    </div>  
+        </div>  
 
 
 </form>
-</x-layout>
 @endsection
