515ab173fb2a18a6e3160114c59660e8f96a6eea
Eduardo de Faria Rios Perucello <115468808+EduardoPerucello@users.noreply.github.com>
2022-12-07 09:42:29 -0300

    Logo que retorna pra view home
    Botão na logo 'ETAVAL'.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+                <a href="/home">
                 <img src="/etavaldeitado.png" alt="logo2" widht=50 height=50>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
