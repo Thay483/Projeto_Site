003064639e42a305ab1747aa236c44d80441a442
Thay483 <112909056+Thay483@users.noreply.github.com>
2022-12-09 12:05:23 -0300

    Alteração dos botões na view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,52 +15,103 @@
                     @endif
 
                     {{ __('Seja bem-vindo!') }}
-@role('secretario')
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="{{ route('alunos.create') }}" role="button">Cadastro de Alunos</a>
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="{{route('cadastrop.create')}}" role="button">Cadastro de Professor</a>
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="/materias" role="button">Cadastro de Cursos</a>
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="{{ route('cadastrop.index') }}" role="button">Lista de Professores</a>
-@endrole
-@role('aluno')
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="/materias" role="button">Lista de Cursos</a>
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="{{route('cursos.index')}}" role="button">Cursos Matriculados</a>
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="{{route('notas.create')}}" role="button">Notas dos Alunos</a>
-    <break></break>
-    <h1></h1>
-@endrole
-@role('professor')
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="{{route('professores.index')}}" role="button">Cursos do Professor</a>
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="#" role="button">Perfil do Professor</a>
-    <break></break>
-    <h1></h1>
-    <a class="btn btn-primary" href="{{ route('alunos.index') }}" role="button">Lista de Alunos</a>
-    <break></break>
-    <h1></h1>
-    
-    <break></break>
-    <h1></h1>
+                </div>
+    @role('admin')
+    <div class="container"> 
+        <div class="row justify-content-center">       
+            <a class="btn btn-primary" href="{{ route('alunos.create') }}" role="button">Cadastro de Alunos</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="{{route('cadastrop.create')}}" role="button">Cadastro de Professor</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="/materias" role="button">Cadastro de Cursos</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="{{ route('cadastrop.index') }}" role="button">Lista de Professores</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="/materias" role="button">Lista de Cursos</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="{{route('cursos.index')}}" role="button">Cursos Matriculados</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="{{route('notas.create')}}" role="button">Notas dos Alunos</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="{{route('professores.index')}}" role="button">Cursos do Professor</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="#" role="button">Perfil do Professor</a>
+            <break></break>
+            <h1></h1>
+            
+            <a class="btn btn-primary" href="{{ route('alunos.index') }}" role="button">Lista de Alunos</a>
+            <break></break>
+            <h1></h1>
+        </div>
+    </div>
+    @endrole
+    @role('secretario')
+    <div class="container"> 
+        <div class="row justify-content-center">         
+            <a class="btn btn-primary" href="{{ route('alunos.create') }}" role="button">Cadastro de Alunos</a>
+            <break></break>
+            <h1></h1>
+            <a class="btn btn-primary" href="{{route('cadastrop.create')}}" role="button">Cadastro de Professor</a>
+            <break></break>
+            <h1></h1>
+            <a class="btn btn-primary" href="/materias" role="button">Cadastro de Cursos</a>
+            <break></break>
+            <h1></h1>
+            <a class="btn btn-primary" href="{{ route('cadastrop.index') }}" role="button">Lista de Professores</a>
+            <break></break>
+            <h1></h1>
+        </div>
+    </div>
+    @endrole
+    @role('aluno')
+    <div class="container"> 
+        <div class="row justify-content-center">
+            <a class="btn btn-primary" href="/materias" role="button">Lista de Cursos</a>
+            <break></break>
+            <h1></h1>
+            <a class="btn btn-primary" href="{{route('cursos.index')}}" role="button">Cursos Matriculados</a>
+            <break></break>
+            <h1></h1>
+            <a class="btn btn-primary" href="{{route('notas.create')}}" role="button">Notas dos Alunos</a>
+            <break></break>
+            <h1></h1>
+        </div>
+    </div>
+    @endrole
+    @role('professor')
+    <div class="container"> 
+        <div class="row justify-content-center">
+            <a class="btn btn-primary" href="{{route('professores.index')}}" role="button">Cursos do Professor</a>
+            <break></break>
+            <h1></h1>
+            <a class="btn btn-primary" href="#" role="button">Perfil do Professor</a>
+            <break></break>
+            <h1></h1>
+            <a class="btn btn-primary" href="{{ route('alunos.index') }}" role="button">Lista de Alunos</a>
+            <break></break>
+            <h1></h1>
+        </div>
+    </div>
+    @endrole
     </div>
             </div>
         </div>
     </div>
-@endrole
 </div>
 @endsection
